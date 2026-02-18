b40e6b1a1b26c770bc87572c01cb5a68fd7fb75f
mdaffabdul <242310004@student.ibik.ac.id>
2026-02-18 22:42:49 +0700

    Intro React

---
+++ Day 17/my-first-react/src/App.tsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div className="flex bg-black min-h-screen justify-center items-center font-serif">
+      <div className="bg-white grid grid-cols-1">
+        <div className="flex flex-row items-center justify-between p-6 gap-48">
+          <p>242310004</p>
+          <p>Institut Bisnis dan Informatika Kesatuan</p>
+        </div>
+        <h1 className="text-center pt-24 font-semibold text-2xl">Muhammad Daffa Abdul Mufid</h1>
+        <h1 className="text-center pt-2 font-semibold pb-24">Mahasiswa</h1>
+        <p className="p-6">Jl.Rangga Gading No.01, Gudang, Kecamatan Bogor Tengah, Kota Bogor, Jawa Barat 16123</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
